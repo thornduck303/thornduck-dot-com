c5cf3801eb9685b826d9f683f6734d4ae157d861
Murilo M. Grosso <murilogrosso@hotmail.com>
2026-03-23 21:21:35 -0300

    Header update

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const logo = document.getElementById("main-logo");
+const header = document.getElementById("main-header");
+const headerButtons = document.getElementById("header-buttons");
 
 window.addEventListener("scroll", () => {
-	if (!logo) return;
+	if (!logo || !header || !headerButtons) return;
 
 	const scroll = window.scrollY;
-	const progress = Math.min(scroll / 100, 1);
+	const progress = Math.min(scroll / 300, 1);
 
 	const scale = 1 - (0.5 * progress);
 	const translateX = -50 * progress;
+	const translateY = -25 * progress;
 	const clip = 100 - (80 * progress);
 
-	logo.style.transform = `translateX(${translateX}%) scale(${scale})`;
+	logo.style.transform = `translateX(${translateX}%) translateY(${translateY}%) scale(${scale})`;
 	logo.style.clipPath = `circle(${clip}% at 35% 30%)`;
+
+	const height = 430 - 300 * progress;
+
+	header.style.height = `${height}px`;
+
+	const buttonHeight = 340 - 300 * progress;
+
+	headerButtons.style.top = `${buttonHeight}px`;
 })
